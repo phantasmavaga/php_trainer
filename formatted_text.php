b4efccedb33e7ad65192faf427ee8eef06a53dfc
phantasmavaga <184984789+phantasmavaga@users.noreply.github.com>
2025-09-10 17:47:52 +0000

    Update formatted_text.php
    Add initial implementations for text formatting, string manipulation

--- formatted_text.php
+++ formatted_text.php
@@ -1,20 +1,20 @@
 <?php
-// printf: Ekrana yazdırır
+// printf: prints to the screen
 printf("1. printf: Merhaba %s, yaşınız %d \n", "Ali", 30);
 
-// sprintf: String olarak döndürür
+// sprintf: Turns a string 
 $mesaj = sprintf("2. sprintf: Merhaba %s, yaşınız %d \n", "Ayşe", 25);
 echo $mesaj;
 
-// fprintf: Dosyaya yazma
+// fprintf: Writes to file
 $dosya = fopen("log.txt", "w");
 fprintf($dosya, "3. fprintf: Log - Kullanıcı: %s, ID: %d\n", "Mehmet", 101);
 fclose($dosya);
 
-// vprintf: Argümanları dizi olarak alır ve ekrana yazdırır
+// vprintf:Takes arguments as a string and prints them to the screen
 vprintf("4. vprintf: Ad: %s, Yaş: %d\n", ["Zeynep", 22]);
 
-// vsprintf: Argümanları dizi olarak alır ve string döndürür
+// vsprintf: Takes arguments as an array and returns a string
 $format = "5. vsprintf: %s - %s - %d\n";
 $degerler = ["Ürün", "Kod123", 999];
 echo vsprintf($format, $degerler);
